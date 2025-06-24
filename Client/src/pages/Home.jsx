c20f93f7583587b1b10e8778265e288006b6be83
VenkyGandrothu <gandrothuvenky@gmail.com>
2025-06-24 17:30:13 +0530

    Bg remover first commit

---
+++ Client/src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from '../components/Header.jsx'
+import Steps from '../components/Steps.jsx'
+import BgSlider from '../components/BgSlider.jsx'
+import Testimonials from '../components/Testimonials.jsx'
+import Upload from '../components/Upload.jsx'
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <Steps />
+      <BgSlider />
+      <Testimonials />
+      <Upload/>
+   </div>
+  )
+}
+
+export default Home
